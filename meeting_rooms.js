c5be97ab2e1912515165e7d49067a45de04740b1
maryhalv <39344430+maryhalv@users.noreply.github.com>
2018-06-29 15:31:30 -0400

    currently uses test date by default

--- meeting_rooms.js
+++ meeting_rooms.js
@@ -11,29 +11,33 @@ var time;
         
     $(document).ready( function() {
 		
-tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+	tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+	tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
 function GetClock(){
-var d=new Date();
-var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
-var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+	var d=new Date();
+	var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
+	var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
 
-if(nhour==0){ap=" AM";nhour=12;}
-else if(nhour<12){ap=" AM";}
-else if(nhour==12){ap=" PM";}
-else if(nhour>12){ap=" PM";nhour-=12;}
+	if(nhour==0){ap=" AM";nhour=12;}
+		else if(nhour<12){ap=" AM";}
+		else if(nhour==12){ap=" PM";}
+		else if(nhour>12){ap=" PM";nhour-=12;}
 
-if(nmin<=9) nmin="0"+nmin;
-if(nsec<=9) nsec="0"+nsec;
+		if(nmin<=9) nmin="0"+nmin;
+		if(nsec<=9) nsec="0"+nsec;
 
 document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
 }
 
-window.onload=function(){
-GetClock();
-setInterval(GetClock,1000);
-}
+	window.onload=function(){
+	GetClock();
+	setInterval(GetClock,1000);
+	};
+
+ setTimeout(function(){
+      window.location.href = window.location.href;
+  },(60000 * 30));
 
 			today = new Date(2018, 06, 25);
 			time  = today.getTime(); //results in milliseconds
@@ -58,6 +62,7 @@ setInterval(GetClock,1000);
            'Authorization' : 'Basic bWhhbHZvcnNlbjk4QGdtYWlsLmNvbTpCb295YWg5ODE2MjA='
         }
     });
+   
 $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=meeting-room-test-data" ,  function (jsonData)  {
      var count = 0;
     $.each(jsonData.records, function (object, objectData) {
@@ -68,11 +73,8 @@ $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=me
         if(count == jsonData.records.length) {
             start_time.sort();
         }
-		
-		
     });
-    
-    
+
     var index;
 	var accessed = [];
 	for(var x = 0; x < start_time.length; x++) {
@@ -130,50 +132,7 @@ $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=me
                     $(".table").append("<tr><td>" + org + "</td><td>" + start + "</td><td>" + end + "</td><td>" + place +  "</td></tr>");
                 }
     } 
-    
-    
- /* $.each(jsonData.records, function (object, objectData) {
-    console.log(objectData.fields.date);
-                if(parseInt(objectData.fields.date.slice(0,4)) === today.getFullYear() && parseInt(objectData.fields.date.slice(5,7)) === today.getMonth()
-                   && parseInt(objectData.fields.date.slice(8)) === today.getDate() && parseInt(objectData.fields.end_time.slice(15,17)) > minutes) {
-                    
-                    if(objectData.fields.organization !== null && isNaN(objectData)) {
-                        org = objectData.fields.organization;
-                    } else {
-                        org = "N/A";
-                    }
-                    if(objectData.fields.start_time !== null && isNaN(objectData)) {
-						if(parseInt(objectData.fields.start_time.slice(11,13)) > 12) {
-							//handling the military time
-							start = parseInt(objectData.fields.start_time.slice(11,13)) - 12;
-							start = start + ":" + objectData.fields.start_time.slice(14,16);
-						} else {
-                        start = objectData.fields.start_time.slice(11,16);
-						}
-						
-                    } else {
-                        start = "N/A";
-                    }
-                    if(objectData.fields.end_time && isNaN(objectData)) {
-                        end = objectData.fields.end_time.slice(11,16);
-                    } else {
-                        start = "N/A";
-                    }
-                    if(objectData.fields.space_name && isNaN(objectData)) {
-                        //location = objectData.fields.space_name;
-                    } else {
-                        start = "N/A";
-                    }
-                    $(".table").append("<tr><td>" + org + "</td><td>" + start + "</td><td>" + end + "</td></tr>");
-                }
-            });*/
- //reloading the page automatically every 30 minutes
-setInterval(function() {
-                  window.location.reload();
-                }, (60000 * 30)); 
 });
-
-		
         }
 		
 		
